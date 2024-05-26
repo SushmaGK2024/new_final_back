b71fba0bdd0b46eb79fbf39fe3ae0556d9a249e7
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-26 06:06:01 +0530

    port

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,11 @@ const mysql = require('mysql2');
 const nodemailer = require('nodemailer');
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || '3306',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mydatabase'
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT ,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   connectionLimit: 10, // Adjust this based on your needs
 });
 function authenticateToken(req, res, next) {
